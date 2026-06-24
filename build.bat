3c86ba6b830432763686a8774e1eacd993c38b1a
Pongsakorn Onnim <pongsakorn.onnim@gmail.com>
2026-06-24 15:01:38 +0700

    Rename exe to risk_report_generator

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Building run_report.exe ...
 echo.
-.venv\Scripts\pyinstaller --onefile --name run_report --collect-all questionary launcher.py
+.venv\Scripts\pyinstaller --onefile --name risk_report_generator --collect-all questionary launcher.py
 echo.
-echo Done! Copy  dist\run_report.exe  into the OneDrive project folder.
+echo Done! Copy  dist\risk_report_generator.exe  into the OneDrive project folder.
 pause
